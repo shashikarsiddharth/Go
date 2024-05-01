ffbcf4f567ecd317876df8a947e9cd13869da693
siddharthshashikar <shashikarsidharth@gmail.com>
2024-05-01 14:34:55 +0530

    Fix 005 code

--- 100-golang-exercises/005/main.go
+++ 100-golang-exercises/005/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+type MyIO struct{}
+
+type IO interface {
+	ReadString() string
+	PrintString(input string)
+}
+
+func (io MyIO) ReadString() string {
+	var input string
+	fmt.Scan(&input)
+	return input
+}
+
+func (io MyIO) PrintString(input string) {
+	fmt.Println(input)
+}
+
 func main() {
-	Hello()
+	var io IO = MyIO{}
+	inp := io.ReadString()
+	io.PrintString(inp)
 }
